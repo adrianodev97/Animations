dcbd9331c1085a41d83f79316fb2ef4f6a9d89e3
Adriano Junior <88254828+adrianodev97@users.noreply.github.com>
2024-08-30 02:12:05 -0300

    feat: add styles and actions to handle slider

---
+++ 3D Infiinite Slider/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let prev = document.querySelector('.prev');
+  let next = document.querySelector('.next');
+
+  next.addEventListener('click', () => {
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.box').appendChild(items[0]);
+  })
+
+  prev.addEventListener('click', () => {
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.box').prepend(items[items.length - 1]);
+  })
+});
