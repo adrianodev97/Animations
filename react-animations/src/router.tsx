16944a62b722d9d3192c0ba9c26d775f87606992
Adriano Junior <88254828+adrianodev97@users.noreply.github.com>
2024-10-05 13:11:53 -0300

    fix: remove non-existent route

--- react-animations/src/router.tsx
+++ react-animations/src/router.tsx
@@ -1,14 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import Teste from "./animations/teste";
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
-	},
-	{
-		path: "/teste",
-		element: <Teste />,
 	},
 ]);
